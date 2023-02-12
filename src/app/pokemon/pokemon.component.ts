246c836fae938f5621ca94124ca09a3213c8d8e5
Szép Gergely <szep.gergely@greenformatics.hu>
2023-02-13 00:29:14 +0100

    connect service into pokemon component

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from '../shared/models/pokemon.model';
-import { MainService } from '../shared/services/main.service';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { PokemonService } from '../shared/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -9,14 +8,62 @@ import { MainService } from '../shared/services/main.service';
 })
 export class PokemonComponent implements OnInit {
 
-  pokemon: Pokemon[] = [];
+  offset = 0;
+  pokemonList: any[] = [];
+  searchData = '';
 
   constructor(
-    private mainService: MainService,
-  ) {
+    private pokemonService: PokemonService,
+  ) {}
 
-  }
   ngOnInit(): void {
-    this.mainService.getAllPokemon().subscribe(data => this.pokemon = data);
+    this.getPokemonData();
+  }
+
+  getPokemonData() {
+    this.pokemonService.getPokemons(this.offset).subscribe(
+      (data: any) => {
+        data.results.forEach((pokemon: any) => {
+          this.pokemonService.getPokemonData(pokemon.url).subscribe(
+            (pokeData: any) => {
+              this.pokemonList.push(pokeData);
+            }
+          )
+        });
+        this.offset += 50;
+      }
+    )
+  }
+
+  getMoreData(){
+    this.getPokemonData();
+  }
+
+  searchPokemon() {
+    if (this.searchData.length > 0) {
+      this.defaultSettings();
+      this.pokemonService.searchPokemon(this.searchData).subscribe(
+        data => this.pokemonList.push(data)
+      )
+    }
+    
+  }
+
+  resetData() {
+    this.defaultSettings();
+    this.searchData = '';
+    this.getPokemonData();
+  }
+
+  defaultSettings() {
+    this.offset = 0;
+    this.pokemonList = [];
+  }
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight - 1) {
+      this.getMoreData();
+    }
   }
 }

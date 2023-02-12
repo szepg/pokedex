bbc155385adb0ba9fdb3229028aac6e97aa99c5b
Szép Gergely <szep.gergely@greenformatics.hu>
2023-02-13 00:28:43 +0100

    create pokemon service

---
+++ src/app/shared/services/pokemon.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/app/environment/env';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getPokemons(offset: number) {
+    return this.http.get(this.apiUrl + 'pokemon?limit=50&offset=' + offset)
+  }
+
+  public getPokemonData(url: string) {
+    return this.http.get(url);
+  }
+
+  public searchPokemon(input: string|number) {
+    return this.http.get(this.apiUrl + 'pokemon/' + input);
+  }
+
+}
